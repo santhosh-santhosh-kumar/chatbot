eb89a8524dd00b4e2f4bb7150daa366c93aaa41c
santhosh kumar <35756354+santhosh-santhosh-kumar@users.noreply.github.com>
2025-04-03 21:34:37 +0530

    color mode change

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -6,13 +6,16 @@ import { FaRocketchat } from "react-icons/fa";
 import { IoSend } from "react-icons/io5";
 import { FaPlay } from "react-icons/fa6";
 import { FaPause } from "react-icons/fa6";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
 
 const User = () => {
   const dispatch = useDispatch();
   const chatContainerRef = useRef(null);
-  
+
   const messageDetails = useSelector(chatDetails);
   console.log(messageDetails);
+  const [ismode,setIsMode]=useState(false)
   const formik = useFormik({
     initialValues: {
       text: "",
@@ -30,40 +33,57 @@ const User = () => {
     },
   });
 
-  const [isPlaying, setIsPlaying] = useState(localStorage.getItem("playingId") || "");
-  const handlePlay = (text,id) => {
-    
+  const [isPlaying, setIsPlaying] = useState(
+    localStorage.getItem("playingId") || ""
+  );
+  const handlePlay = (text, id) => {
     const value = new SpeechSynthesisUtterance(text);
     value.onend = () => {
       setIsPlaying("");
       localStorage.removeItem("playingId");
-    };   
+    };
     if (!isPlaying) {
       localStorage.removeItem("playingId");
       window.speechSynthesis.cancel();
       window.speechSynthesis.speak(value);
-      setIsPlaying(id)
+      setIsPlaying(id);
     } else {
       window.speechSynthesis.cancel();
-      setIsPlaying("")
+      setIsPlaying("");
     }
   };
-  
+
   useEffect(() => {
     if (chatContainerRef.current) {
-      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
     }
   }, [messageDetails]);
+
+  
   return (
     <div>
-      <div className="w-96  lg:rounded-t-lg  shadow-xl">
+      <div className="w-96  lg:rounded-t-lg  shadow-xl lg:mt-10">
         <div className="">
-          <div className="bg-violet-600 flex justify-between lg:px-2 px-4 py-4 text-white text-2xl lg:rounded-t-lg ">
-            <p className="font-bold">Chatbot</p>
-            <FaRocketchat />
-          </div>
+          <div className="flex justify-between items-center bg-violet-600 px-4">
+            <div className="bg-violet-600 flex items-center gap-4 lg:px-2  py-4 text-white text-2xl lg:rounded-t-lg ">
+              <FaRocketchat size={30}/>
+              <p className="font-bold">Chatbot</p>
+            </div>
 
-          <div   ref={chatContainerRef} className="py-4 lg:h-96 h-[540px] overflow-auto">
+           <div>
+           <div className={`${ismode ? "hidden" : "block"} text-3xl text-white`} onClick={()=>setIsMode(true)}>
+              <MdOutlineDarkMode />
+            </div>
+            <div className={`${ismode ? "block" : "hidden"} text-3xl text-white`} onClick={()=>setIsMode(false)}>
+            <MdDarkMode />
+            </div>
+           </div>
+          </div>
+          <div
+            ref={chatContainerRef}
+            className={`${ismode ? "bg-black" :"bg-white"} py-4 lg:h-96 h-[540px] overflow-auto`}
+          >
             {messageDetails.map((value) => {
               return (
                 <>
@@ -87,18 +107,22 @@ const User = () => {
                         </p>
                       </p>
                       <div>
-                      <p
-                        className={`${isPlaying == value.id ? "hidden" : "block"}`}
-                        onClick={() => handlePlay(value.text,value.id)}
-                      >
-                        <FaPlay className="text-gray-700"/>
-                      </p>
-                      <p
-                        className={`${isPlaying == value.id ? "block" : "hidden"}`}
-                        onClick={() => handlePlay(value.text,value.id)}
-                      >
-                        <FaPause className="text-gray-700"/>
-                      </p>
+                        <p
+                          className={`${
+                            isPlaying == value.id ? "hidden" : "block"
+                          }`}
+                          onClick={() => handlePlay(value.text, value.id)}
+                        >
+                          <FaPlay className="text-gray-700" />
+                        </p>
+                        <p
+                          className={`${
+                            isPlaying == value.id ? "block" : "hidden"
+                          }`}
+                          onClick={() => handlePlay(value.text, value.id)}
+                        >
+                          <FaPause className="text-gray-700" />
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -107,7 +131,7 @@ const User = () => {
             })}
           </div>
         </div>
-        <form onSubmit={formik.handleSubmit} className="py-4">
+        <form onSubmit={formik.handleSubmit} className={`${ismode ? "bg-black" :"bg-white"} py-4`}>
           <div className="flex justify-center items-center px-4 rounded-lg gap-3">
             <textarea
               type="text"
